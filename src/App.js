6e9ca7ab0813a54357610a6a0c7c40dfb11bcf98
qaws7791 <qaws7791@gmail.com>
2021-12-24 14:31:26 +0900

    meta태그 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <Header></Header>
       <Routess></Routess>
     </div>
