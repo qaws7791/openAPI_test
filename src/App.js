3756031b27e1bbc9311ebd1d983fb44fbf5dab0c
qaws7791 <qaws7791@gmail.com>
2021-12-24 14:45:39 +0900

    http를 https로 변경

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <Header></Header>
       <Routess></Routess>
     </div>
